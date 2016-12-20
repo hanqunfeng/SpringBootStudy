b3b8e29f53d9525f06ecf4f31705006ef24006f4
hanqf <hanqf2008@163.com>
2016-12-20 10:13:12 +0800

    mybatis demo commit

---
+++ mybatissingledatasource/src/test/java/com/example/MybatisSingleDatasourceApplicationTests.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import com.example.pojo.Person;
+import com.example.service.PersonService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MybatisSingleDatasourceApplicationTests {
+
+	@Autowired
+	PersonService personService;
+
+	@Test
+	public void selectByPrimaryKey(){
+		Person person = personService.selectByPrimaryKey(1);
+		System.out.println(person);
+	}
+
+	@Test
+	public void insert(){
+		Person person = new Person();
+		person.setpName("王五");
+		person.setpAge(18);
+		System.out.println(personService.insert(person));
+
+	}
+
+
+}
