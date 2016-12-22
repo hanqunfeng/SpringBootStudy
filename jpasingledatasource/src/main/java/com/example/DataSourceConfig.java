df2a284a41446dc35d37d53678145e6dbd168171
hanqf <hanqf2008@163.com>
2016-12-22 19:14:25 +0800

    jpa示例

---
+++ jpasingledatasource/src/main/java/com/example/DataSourceConfig.java
@@ -0,0 +1,82 @@
+package com.example;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * ${DESCRIPTION}
+ * Created by hanqunfeng on 2016/12/15 18:01.
+ */
+
+@Configuration
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableJpaRepositories(basePackages = "com.example.dao", entityManagerFactoryRef = "entityManagerFactory", transactionManagerRef = "transactionManager")
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driverClass;
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String userName;
+    @Value("${spring.datasource.password}")
+    String passWord;
+
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(passWord);
+        return dataSource;
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setDataSource(dataSource());
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return jpaTransactionManager;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(true);
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setGenerateDdl(true);
+        return adapter;
+    }
+
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource());
+        entityManager.setJpaVendorAdapter(jpaVendorAdapter());
+        entityManager.setPackagesToScan("com.example.model");// entity package
+        entityManager.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        //Properties properties = new Properties();
+        //properties.put("javax.persistence.transactionType", "JTA");
+        //entityManager.setJpaProperties(properties);
+        return entityManager;
+
+    }
+
+}
--- jpasingledatasource/src/main/java/com/example/JpaSingleDatasourceApplication.java
+++ jpasingledatasource/src/main/java/com/example/JpaSingleDatasourceApplication.java
@@ -2,8 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class JpaSingleDatasourceApplication {
 
 	public static void main(String[] args) {
