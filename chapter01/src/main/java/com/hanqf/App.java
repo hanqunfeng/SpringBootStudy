e4437f7d3dec4acd8a4f14586c0439aec2853b3a
hanqf <hanqf2008@163.com>
2020-03-02 10:57:03 +0800

    chapter01

--- chapter01/src/main/java/com/hanqf/App.java
+++ chapter01/src/main/java/com/hanqf/App.java
@@ -1,24 +1,32 @@
 package com.hanqf;
 
 import com.hanqf.config.AppConfig;
+import com.hanqf.config.OtherConfig;
 import com.hanqf.model.User1;
 import com.hanqf.model.User2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 /**
  * Hello world!
  */
 public class App {
 
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+
     public static void main(String[] args) {
 
+        //获取注解对象，多个配置类可以逗号分隔
         ApplicationContext ctx
-                = new AnnotationConfigApplicationContext(AppConfig.class);
+                = new AnnotationConfigApplicationContext(AppConfig.class, OtherConfig.class);
         User1 user1 = ctx.getBean(User1.class);
-        System.out.println(user1);
+        logger.info(user1.toString());
 
         User2 user2 = (User2) ctx.getBean("user2");
-        System.out.println(user2);
+        logger.info(user2.toString());
     }
 }
